a2d62ecb361b5cecd7cc6adfe1ef087642b5e542
AnastasiaShumilenko <119425257+AnastasiaShumilenko@users.noreply.github.com>
2022-12-08 14:26:53 +0400

    Add files via upload

---
+++ Task10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.Clear();
+            Console.Write("Введите любое число: ");
+            int a = int.Parse(Console.ReadLine() ?? "0" );
+            if (a < 100)
+            {
+            Console.WriteLine("Число меньше 100!");
+            }
+            else{
+            
+              while (a>1000)
+              {
+                 a/=10;
+              }
+                Console.WriteLine(a%10);
+            }
+        }
+    }
+}
