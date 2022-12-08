1442bc3fc753a877ce952fe0884662d54822a8c6
AnastasiaShumilenko <119425257+AnastasiaShumilenko@users.noreply.github.com>
2022-12-08 14:25:39 +0400

    Add files via upload

---
+++ Task13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+             
+             if ((num > 7) ||  (num < 1))
+             {
+             Console.WriteLine("Неверний день недели");
+             return;
+             }
+
+             if ((num > 5))
+             {
+                Console.WriteLine("Выходной");
+             }
+
+             else
+            {
+             Console.WriteLine("Будний");
+            }
+            
+            
+
+        }
+    }
+}
