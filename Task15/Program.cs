edc79e9f4aa627ffd1eba60187947c13054853d5
AnastasiaShumilenko <119425257+AnastasiaShumilenko@users.noreply.github.com>
2022-12-08 14:27:55 +0400

    Add files via upload

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int num = new Random().Next(100, 1000);
+            int second =  num / 10 % 10;
+            Console.WriteLine($"Вторая цифра в числе: {num} -> {second}");
+        
+    }
+}
+}
